fd6dda281fdce9b030d69708e247ebfeafc8f9b6
k0ofdeath <parth.thaker.311@gmail.com>
2016-05-31 22:11:27 -0400

    Added Hp Bar Sprites

---
+++ New Unity Project/Assets/Scripts/HealthBar.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class HealthBar : MonoBehaviour {
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.transform.position = Input.mousePosition;
+	}
+}
